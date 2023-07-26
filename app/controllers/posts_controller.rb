6b762871e52095a6e473c8214e69bdcad437138c
Mauricio Fernandez <mauricio.fernandezcastro@gmail.com>
2023-07-26 13:52:29 -0400

    v2 correcciones

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :image :user_id)
+      params.require(:post).permit(:title, :description, :image, :user_id)
     end
 end
